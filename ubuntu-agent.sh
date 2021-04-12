f43aca9667748f6d5c6d3efe7a6a6d1d9666f34b
LUUTHANHTU <53068248+luuthanhtu@users.noreply.github.com>
2021-04-12 23:39:21 +0700

    Update ubuntu-agent.sh

--- ubuntu-agent.sh
+++ ubuntu-agent.sh
@@ -5,7 +5,7 @@ echo "Azure_agent	ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
 mkdir /home/AzureAgent/agent && cd /home/AzureAgent/agent
 wget https://vstsagentpackage.azureedge.net/agent/2.184.2/vsts-agent-linux-x64-2.184.2.tar.gz
 tar zxvf vsts-agent-linux-x64-2.184.2.tar.gz
-sudo rm vsts-agent-linux-x64-2.184.2.tar.gz
+# sudo rm vsts-agent-linux-x64-2.184.2.tar.gz
 su - Azure_agent
-./config.sh
-./run.sh
+# ./config.sh
+# ./run.sh
