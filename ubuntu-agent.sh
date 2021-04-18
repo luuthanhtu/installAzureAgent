0ad254941e87f300d765263781f1ab831db40463
LUUTHANHTU <53068248+luuthanhtu@users.noreply.github.com>
2021-04-18 09:23:10 +0700

    Update ubuntu-agent.sh

--- ubuntu-agent.sh
+++ ubuntu-agent.sh
@@ -1,3 +1,15 @@
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+sudo apt -y install nodejs
+sudo apt-get install awscli
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+sudo apt install docker-ce -y
+sudo systectl enable docker
+sudo usermod -aG docker ${USER}
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 sudo useradd -G sudo -s /bin/bash -d /home/AzureAgent Azure_agent
 sudo mkdir -p /home/AzureAgent
 sudo chown -R Azure_agent /home/AzureAgent
