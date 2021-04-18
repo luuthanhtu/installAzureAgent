8d6b788c9d5d20a4195a68f45800ab67adb83681
LUUTHANHTU <53068248+luuthanhtu@users.noreply.github.com>
2021-04-18 09:32:48 +0700

    Update ubuntu-agent.sh

--- ubuntu-agent.sh
+++ ubuntu-agent.sh
@@ -13,12 +13,12 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo useradd -G sudo -s /bin/bash -d /home/AzureAgent Azure_agent
 sudo mkdir -p /home/AzureAgent
 sudo chown -R Azure_agent /home/AzureAgent
-echo "Azure_agent	ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
+echo "Azure_agent ALL=(ALL:ALL) NOPASSWD:ALL" >> /etc/sudoers
 mkdir /home/AzureAgent/agent && cd /home/AzureAgent/agent
 wget https://vstsagentpackage.azureedge.net/agent/2.184.2/vsts-agent-linux-x64-2.184.2.tar.gz
 tar zxvf vsts-agent-linux-x64-2.184.2.tar.gz
 sudo rm vsts-agent-linux-x64-2.184.2.tar.gz
 su - Azure_agent
+sudo chmod 777 *
 cd agent
-./config.sh
-./run.sh
+
