0faf069581ee61ee0a3d8658ca31f8bcfcdf7a94
LUUTHANHTU <53068248+luuthanhtu@users.noreply.github.com>
2021-04-21 22:36:20 +0700

    Update ubuntu-agent.sh

--- ubuntu-agent.sh
+++ ubuntu-agent.sh
@@ -5,7 +5,7 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 sudo apt update
-sudo apt install -y docker-ce -y
+sudo apt install docker-ce -y
 sudo systectl enable docker
 sudo usermod -aG docker ${USER}
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
