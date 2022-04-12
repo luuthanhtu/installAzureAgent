1b2ffc49678041b3947c14adf33bb462a93d5918
LUUTHANHTU <53068248+luuthanhtu@users.noreply.github.com>
2022-04-12 15:07:24 +0700

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -6,5 +7,6 @@ sudo apt update
 sudo apt install docker-ce -y
 sudo systectl enable docker
 sudo usermod -aG docker ${USER}
+sudo chmod 666 /var/run/docker.sock
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
